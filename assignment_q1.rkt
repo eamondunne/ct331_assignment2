3d15c51ca68f292206556b26ddf136dfd54ef537
eamondunne <eamon.dunne@gmail.com>
2017-10-24 16:52:21 +0100

    Assignment 1 Code Written

--- assignment_q1.rkt
+++ assignment_q1.rkt
@@ -1,14 +1,16 @@
 #lang racket
 
 ; cons pair of two numbers
-(cons 1 2)
+(define cons_pair(cons 1 2))
 
 ; a list of 3 numbers, using only the cons function
-(cons 1 (cons 2 3))
+(define cons_list(cons 1 (cons 2 3)))
 
 ; list containing a string, a number & a nested list of three numbers, using only the cons function
-(cons "Hello" (cons 9 (cons 3 1 )))
+(define cons_list2(cons "CONST" (cons 9 (cons 8 (cons 7 6 )))))
 
 ; A list containing a string, a number and a nested list of three numbers, using only the list function.
+(define list_list(list "LIST" 9 (list 8 7 6)))
 
 ;A list containing a string, a number and a nested list of three numbers, using only the append function
+(define append_list(append '(APPEND) '(9) '((8 7 6))))
