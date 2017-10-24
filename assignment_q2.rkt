cfd743ecf2c9d169956834036261930ccb8ee4a6
eamondunne <eamon.dunne@gmail.com>
2017-10-24 17:25:53 +0100

    Assignment 2 Pt A & B

--- assignment_q2.rkt
+++ assignment_q2.rkt
@@ -1,17 +1,13 @@
 #lang racket
 
-;This is an example implementation of ins_beg,
-;It obviously doesn't do what it should, so you
-;can edit this function to get started.
-;
-;Please note the provide function is necessary
-;for the unit tests to work. Please include a
-;(provide) for each function you write in your
-;submitted assignment.
-;
-;You may delete these comments!
-
+;Provide Functions
 (provide ins_beg)
+(provide ins_end)
 
+;A
 (define (ins_beg el lst)
-  (display "Hello, I'm ins_beg!\n"))
+  (cons el lst))
+
+;B
+(define (ins_end el lst)
+  (append lst (list el)))
