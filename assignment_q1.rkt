c6d2d511b07e129e99ad4e78cd766ab7ae274274
eamondunne <eamon.dunne@gmail.com>
2017-10-24 16:51:09 +0100

    Assignment 1 Code Written

--- assignment_q1.rkt
+++ assignment_q1.rkt
@@ -1,1 +1,14 @@
 #lang racket
+
+; cons pair of two numbers
+(cons 1 2)
+
+; a list of 3 numbers, using only the cons function
+(cons 1 (cons 2 3))
+
+; list containing a string, a number & a nested list of three numbers, using only the cons function
+(cons "Hello" (cons 9 (cons 3 1 )))
+
+; A list containing a string, a number and a nested list of three numbers, using only the list function.
+
+;A list containing a string, a number and a nested list of three numbers, using only the append function
