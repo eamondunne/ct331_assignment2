5bde65ee7d7b63cd5a868df31730f92100e5c360
eamondunne <eamon.dunne@gmail.com>
2017-10-25 15:41:22 +0100

    Assignment 2 Part C & D

--- assignment_q2.rkt
+++ assignment_q2.rkt
@@ -3,6 +3,8 @@
 ;Provide Functions
 (provide ins_beg)
 (provide ins_end)
+(provide count_top_level)
+(provide count_instances)
 
 ;A
 (define (ins_beg el lst)
@@ -11,3 +13,25 @@
 ;B
 (define (ins_end el lst)
   (append lst (list el)))
+
+;C
+(define (count_top_level list)
+  (cond ((null? list) 0)
+      ((not (pair? list)) 1)
+      (else (+ (count_top_level (car list))
+                 (count_top_level (cdr list))))
+      )
+)
+
+;D count_instances
+;(define (count_instances item list)
+ ; (cond ((null? list) 0)                 
+  ;      ((equal? item (car list)) (+1 (count_instances item (cdr list))))))
+
+
+(define (count_instances item list) 
+  (cond ((null? list) 0) 
+        ((equal? item (car list)) (+ 1 (count_instances item (cdr list)))) 
+        ( (count_instances item (cdr list))) 
+) 
+)
