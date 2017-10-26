3be25dd873629e84da6514731792a226e97e717c
eamondunne <eamon.dunne@gmail.com>
2017-10-26 15:10:07 +0100

    Q3: Binary Tree setup and traversable

--- assignment_q3.rkt
+++ assignment_q3.rkt
@@ -1,1 +1,19 @@
 #lang racket
+
+(define tree '(((() 5 ()) 10(() 15 ())) 20 ((() 25 ()) 30 (() 35 ()))))
+
+(define (left_child bst)
+  car bst)
+
+(define (right_child bst)
+  caddr bst)
+
+(define (value bst)
+  cadr bst)
+
+(define (findval val bst)
+  (cond
+    [(null? bst) #f]
+    [(equal? val (value bst)) #t]
+  )
+)
