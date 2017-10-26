91dd74c533e57d71f4dc63a3e97a4b701e600952
eamondunne <eamon.dunne@gmail.com>
2017-10-26 14:11:04 +0100

    Question 2 finished

--- assignment_q2.rkt
+++ assignment_q2.rkt
@@ -23,15 +23,29 @@
       )
 )
 
-;D count_instances
-;(define (count_instances item list)
- ; (cond ((null? list) 0)                 
-  ;      ((equal? item (car list)) (+1 (count_instances item (cdr list))))))
-
-
+;D
 (define (count_instances item list) 
   (cond ((null? list) 0) 
         ((equal? item (car list)) (+ 1 (count_instances item (cdr list)))) 
-        ( (count_instances item (cdr list))) 
-) 
+        ((count_instances item (cdr list))) 
+        ) 
 )
+
+;E
+(define (count_instances_tr el lst)
+  (tinst el 0 lst)
+ )
+
+(define (tinst el total lst)
+  (cond ((null? lst)0)
+        ((equal? el (car lst))(+ 1 total(tinst el total (cdr lst))))
+        ((tinst el total (cdr lst)))
+    )
+)
+
+;F
+(define (count_instances_deep el lst)
+  (cond [(empty? lst) 0]
+        [(list? (car lst)) (+ (count_instances_deep el (car lst)) (count_instances_deep el (cdr lst)))]
+        [(equal? el (car lst)) (+ 1 (count_instances_deep el (cdr lst)))]
+        [else (count_instances_deep el (cdr lst))]))
